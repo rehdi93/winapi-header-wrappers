4b44e932def69f0aafcd1b07fd46948766c2b1cf
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2020-01-04 07:12:43 +0100

    Update windows-h.utf8.hpp

--- source/winapi-header-wrappers/windows-h.utf8.hpp
+++ source/winapi-header-wrappers/windows-h.utf8.hpp
@@ -3,4 +3,14 @@
 #define UTF8_WINAPI
 #include "windows-h.hpp"
 
-inline const bool winapi_h_utf8_assertion_dummy = winapi_h_assert_utf8_codepage();
+namespace uuid_0985060C_1AAD_453C_B3F9_A2E543F4CF1E {
+    struct Winapi_envelope
+    {
+        Winapi_envelope()
+        {
+            static const bool dummy = winapi_h_assert_utf8_codepage();
+        }
+    };
+    
+    static const Winapi_envelope ensured_globally_single_utf8_assertion{};
+}  // namespace uuid_0985060C_1AAD_453C_B3F9_A2E543F4CF1E
